b18f5e96fd320e2d881e54664115e9b92702ca85
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-09-26 15:15:40 +0200

    Create #ask_for_finish

--- lib/game.rb
+++ lib/game.rb
@@ -26,4 +26,18 @@ class Game
     end
     start
   end
+
+  def ask_for_finish
+    user_input = false
+    until user_input
+      puts 'Enter the ending position of the knight (a,b format): '
+      finish = gets.chomp.split(',').map(&:to_i)
+      if finish.length != 2
+        puts 'A position is made up of 2 cordinates! Try again.'
+      else
+        user_input = true
+      end
+    end
+    finish
+  end
 end
