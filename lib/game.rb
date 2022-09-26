7156a3fe2e3f92509990c38a4df3ba31cf5d46ba
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-09-26 15:15:08 +0200

    Create #ask_for_start

--- lib/game.rb
+++ lib/game.rb
@@ -12,4 +12,18 @@ class Game
 
     play_game(finish)
   end
+
+  def ask_for_start
+    user_input = false
+    until user_input
+      puts 'Enter the starting position of the knight (a,b format): '
+      start = gets.chomp.split(',').map(&:to_i)
+      if start.length != 2
+        puts 'A position is made up of 2 cordinates! Try again.'
+      else
+        user_input = true
+      end
+    end
+    start
+  end
 end
